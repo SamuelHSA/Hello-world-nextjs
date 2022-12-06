f8147df9a8f73e48cd1b2c28f37bc69e026c7fb2
SamuelHSA <longimusplay@gmail.com>
2022-12-06 20:16:42 -0300

    Inicio do projeto

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+const x = {
+  name: 'Samuel',
+  idade: '15'
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.method)
+  res.status(200).json(x)
 }
