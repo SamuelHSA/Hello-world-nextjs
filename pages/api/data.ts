06f5d2c10820c1c138be9b249c80d06b9fefc462
SamuelHSA <longimusplay@gmail.com>
2022-12-07 18:25:44 -0300

    Termino da aula de Next.js
    Aula da Devaria

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import moment from 'moment';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (req : NextApiRequest, res : NextApiResponse) =>{
-    res.status(200).json({ data : new Date() });
+    res.status(200).json({ data : moment().format('DD/MM/yyyy   HH:mm:ss') });
 }
